741ae5c50548f120377621bdf94ef9a68ec41dc8
Adarsh Varma <adarshvarma7805@gmail.com>
2026-05-02 16:15:45 +0530

    Fix 500 error on signup by creating uploads folder

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,6 +1,13 @@
 import multer from "multer";
+import fs from "fs";
+
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads"),
+  destination: (req, file, cb) => {
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
+    cb(null, "uploads");
+  },
   filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
 })
 
